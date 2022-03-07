54e68da2365abab7ceeff75a6d1d020e38387aaf
Suraj Garud <surajgarud95@gmail.com>
2022-03-07 14:16:07 +0530

    Employee Payroll Using Thread

---
+++ EmployeePayrollUsingThreads/Demo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingThreads
+{
+    internal class Demo
+    {
+        public static void GetMostCommonWords(string[] words)
+        {
+            var frequencyOrder = from word in words
+                                 where word.Length > 6
+                                 group word by word into q
+                                 orderby q.Count() descending
+                                 select q.Key;
+            var commonWord = frequencyOrder.Take(10);
+            //Console.WriteLine($"The most common word is {commonWord}");
+            Console.ReadKey();
+        }
+
+        public static string[] CreateWordArray(string uri)
+        {
+            Console.WriteLine($"Retrieving from {uri}");
+            //Download a web page the easy way
+            string blog = new WebClient().DownloadString(uri);
+            return blog.Split(
+                new char[] { ' ', '\u000A', '.', ',', '-', '_', '/' },
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+        public static string GetLongestWord(string[] words)
+        {
+            string longestWord = (from w in words
+                                  orderby w.Length descending
+                                  select w).First();
+
+            Console.WriteLine($"Task 1 -- The longest word is {longestWord}.");
+            return longestWord;
+        }
+
+    }
+}
---
+++ EmployeePayrollUsingThreads/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeePayrollUsingThreads;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Employee Payroll using Threads");
+string[] words =Demo.CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+#region ParallelTasks
+Parallel.Invoke(() =>
+{
+    Console.WriteLine("Begin first task...");
+    Demo.GetLongestWord(words);
+},// close 1st action
+() =>
+{
+    Console.WriteLine("Begin second task...");
+    Demo.GetMostCommonWords(words);
+},
+() =>
+{
+    Console.WriteLine("Begin third task...");
+    Demo.GetMostCommonWords(words);
+}// close third action
+);// close parallel.invoke
+#endregion
+//Console.ReadKey();
+
